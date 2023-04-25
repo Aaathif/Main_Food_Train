e0f609a464d20ec7296c20890f02bec3f8f1f059
Aathif <aaathif115@gmail.com>
2023-04-25 12:45:07 +0530

    first commit for admin

---
+++ src/datatablesource.js
@@ -0,0 +1,155 @@
+export const userColumns = [
+  { field: "_id", headerName: "ID", width: 70 },
+  {
+    field: "user",
+    headerName: "User",
+    width: 230,
+    renderCell: (params) => {
+      return (
+        <div className="cellWithImg">
+          <img className="cellImg" src={params.row.img || "https://i.ibb.co/MBtjqXQ/no-avatar.gif"} alt="avatar" />
+          {params.row.username}
+        </div>
+      );
+    },
+  },
+  {
+    field: "email",
+    headerName: "Email",
+    width: 230,
+  },
+
+  {
+    field: "country",
+    headerName: "Country",
+    width: 100,
+  },
+  {
+    field: "city",
+    headerName: "City",
+    width: 100,
+  },
+  {
+    field: "phone",
+    headerName: "Phone",
+    width: 100,
+  },
+];
+
+{/*------------------------ hotelColumns --------------------------*/}
+
+export const hotelColumns = [
+  { 
+    field: "_id", 
+    headerName: "ID", 
+    width: 250 
+  },
+  {
+    field: "name",
+    headerName: "Name",
+    width: 150,
+    
+  },
+  {
+    field: "type",
+    headerName: "Type",
+    width: 100,
+  },
+  {
+    field: "title",
+    headerName: "Title",
+    width: 230,
+  },
+  {
+    field: "city",
+    headerName: "City",
+    width: 100,
+  },
+];
+
+{/*------------------------ inventoryColumns --------------------------*/}
+
+export const inventoryColumns = [
+  { 
+    field: "_id", 
+    headerName: "ID", 
+    width: 270 
+  },
+  {
+    field: "date",
+    headerName: "Date",
+    width: 120,
+  },
+  {
+    field: "title",
+    headerName: "Title",
+    width: 300,
+    renderCell: (params) => {
+      return (
+        <div className="cellWithImg">
+          <img className="cellImg" src={params.row.photos || "https://i.ibb.co/MBtjqXQ/no-avatar.gif"} alt="avatar" />
+          {params.row.title}
+        </div>
+      );
+    },
+    
+    
+  },
+  {
+    field: "description",
+    headerName: "Description",
+    width: 220,
+  },
+  {
+    field: "category",
+    headerName: "Category",
+    width: 150,
+  },
+  {
+    field: "prices",
+    headerName: "Price",
+    width: 150,
+  },
+  // {
+  //   field: "quantity",
+  //   headerName: "Quantity",
+  //   width: 100,
+  // },
+];
+
+{/*------------------------ MenuListColumns --------------------------*/}
+
+
+export const MenuListColumns = [
+  {
+    field: "_id",
+    headerName: "ID",
+    width: 70,
+  },
+  {
+    field: "categoryTitle",
+    headerName: "Category Title",
+    width: 250,
+  },
+  {
+    field: "offerTag",
+    headerName: "Offer Percentage",
+    width: 120,
+  },
+  {
+    field: "title",
+    headerName: "Title",
+    width: 300,
+  },
+  {
+    field: "price",
+    headerName: "Price",
+    width: 120,
+  },
+  {
+    field: "description",
+    headerName: "Description",
+    width: 300,
+  }
+]
+
---
+++ src/formSource.js
@@ -0,0 +1,168 @@
+export const userInputs = [
+  {
+    id: "username",
+    label: "Username",
+    type: "text",
+    placeholder: "john_doe",
+  },
+  {
+    id: "email",
+    label: "Email",
+    type: "email",
+    placeholder: "john_doe@gmail.com",
+  },
+  {
+    id: "phone",
+    label: "Phone",
+    type: "text",
+    placeholder: "+1 234 567 89",
+  },
+  {
+    id: "password",
+    label: "Password",
+    type: "password",
+  },
+  {
+    id: "country",
+    label: "Country",
+    type: "text",
+    placeholder: "USA",
+  },
+  {
+    id: "city",
+    label: "City",
+    type: "text",
+    placeholder: "USA",
+  },
+];
+
+export const productInputs = [
+  {
+    id: "title",
+    label: "Title",
+    type: "text",
+    placeholder: "Chicken Pizza",
+  },
+  {
+    id: "category",
+    label: "Category",
+    type: "text",
+    placeholder: "Candy",
+  },
+  {
+    id: "type",
+    label: "type",
+    type: "text",
+    placeholder: "Burger",
+  },
+  {
+    id: "date",
+    label: "Date",
+    type: "text",
+    placeholder: "2023.04.31",
+  },
+  {
+    id: "description",
+    label: "Description",
+    type: "text",
+    placeholder: "Description",
+  },
+  // {
+  //   id: "size",
+  //   label: "Size",
+  //   type: "text",
+  //   placeholder: "S or M or L",
+  // },
+  // {
+  //   id: "prices",
+  //   label: "Price",
+  //   type: "text",
+  //   placeholder: "100",
+  // },
+];
+
+// -------------------------------- hotelInputs --------------------------------------
+export const hotelInputs = [
+  {
+    id: "name",
+    label: "Name",
+    type: "text",
+    placeholder: "My Hotel",
+  },
+  {
+    id: "type",
+    label: "Type",
+    type: "text",
+    placeholder: "hotel",
+  },
+  {
+    id: "city",
+    label: "City",
+    type: "text",
+    placeholder: "New York",
+  },
+  {
+    id: "address",
+    label: "Address",
+    type: "text",
+    placeholder: "elton st, 216",
+  },
+  {
+    id: "distance",
+    label: "Distance from City Center",
+    type: "text",
+    placeholder: "500",
+  },
+  {
+    id: "title",
+    label: "Title",
+    type: "text",
+    placeholder: "The best Hotel",
+  },
+  {
+    id: "desc",
+    label: "Description",
+    type: "text",
+    placeholder: "description",
+  },
+  {
+    id: "cheapestPrice",
+    label: "Price",
+    type: "text",
+    placeholder: "100",
+  },
+];
+
+// ----------------------------- menuListInput ---------------------------------------- 
+export const menuListInput = [
+  {
+    id: "categoryTitle",
+    label: "Category Title",
+    type: "text",
+    placeholder: "Pizza",
+  },
+  {
+    id: "offerTag",
+    label: "Offer Percentage(%)",
+    type: "number",
+    placeholder: "20",
+  },
+  {
+    id: "title",
+    label: "Title",
+    type: "text",
+    placeholder: "Chicken Pizza",
+  },
+  {
+    id: "price",
+    label: "Price",
+    type: "text",
+    placeholder: "2250",
+  },
+  {
+    id: "description",
+    label: "Description",
+    type: "text",
+    placeholder: "Dressed with leaf lettuce and tomatoes on a toasted..."
+  }
+]
