6faec6e5635aeb43b3aa91977504e3e89f886b68
Sathurjan <it21188718@my.sliit.lk>
2023-04-28 09:19:03 +0530

    sathurjjjan committt

--- main/src/context/AuthContext.js
+++ main/src/context/AuthContext.js
@@ -4,7 +4,7 @@ const INITIAL_STATE = {
   user: JSON.parse(localStorage.getItem("user")) || null,
   loading: false,
   error: null,
-};
+}; //ffdd
 //gggg
 export const AuthContext = createContext(INITIAL_STATE);
 
