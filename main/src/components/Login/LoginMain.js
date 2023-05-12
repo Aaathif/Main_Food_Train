defb96ef3c6d760a011b35b95aafef044cbdfdc8
IT21188718 <IT21188718@my.sliit.lk>
2023-05-12 11:41:46 +0530

    code formating

--- main/src/components/Login/LoginMain.js
+++ main/src/components/Login/LoginMain.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import data from './LoginData'
-import Login from './Login'
+import React from "react";
+import data from "./LoginData";
+import Login from "./Login";
 function LoginMain() {
   return (
     <div>
-        <div>
-            {data.map(Data => { 
-                return <div><Login Data ={Data}/></div>
-
-            })}
-        </div>
+      <div>
+        {data.map((Data) => {
+          return (
+            <div>
+              <Login Data={Data} />
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
-export default LoginMain
+export default LoginMain;
