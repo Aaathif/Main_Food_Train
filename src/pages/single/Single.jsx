3015dc170b06125132cfc583e0ab17f1550124ec
Aathif <aaathif115@gmail.com>
2023-05-17 05:39:55 +0530

    after product single in admin

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -3,8 +3,19 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Chart from "../../components/chart/Chart";
 import List from "../../components/table/Table";
+import { useLocation } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
 
 const Single = () => {
+
+
+  const location = useLocation()
+  const ids = location.pathname.split("/")[2]
+  console.log(location)
+  console.log(ids)
+
+  const { data, loading, error } = useFetch(`http://localhost:8000/api/product/find/${ids}`);
+
   return (
     <div className="single">
       <Sidebar />
@@ -16,29 +27,25 @@ const Single = () => {
             <h1 className="title">Information</h1>
             <div className="item">
               <img
-                src="https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260"
+                src={data.photos}
                 alt=""
                 className="itemImg"
               />
               <div className="details">
-                <h1 className="itemTitle">Jane Doe</h1>
+                <h1 className="itemTitle">{data.title}</h1>
                 <div className="detailItem">
-                  <span className="itemKey">Email:</span>
-                  <span className="itemValue">janedoe@gmail.com</span>
+                  <span className="itemKey">Category:</span>
+                  <span className="itemValue">{data.category}</span>
                 </div>
                 <div className="detailItem">
-                  <span className="itemKey">Phone:</span>
-                  <span className="itemValue">+1 2345 67 89</span>
+                  <span className="itemKey">Type:</span>
+                  <span className="itemValue">{data.type}</span>
                 </div>
                 <div className="detailItem">
-                  <span className="itemKey">Address:</span>
+                  <span className="itemKey">Description:</span>
                   <span className="itemValue">
-                    Elton St. 234 Garden Yd. NewYork
+                    {data.description}
                   </span>
-                </div>
-                <div className="detailItem">
-                  <span className="itemKey">Country:</span>
-                  <span className="itemValue">USA</span>
                 </div>
               </div>
             </div>
@@ -49,7 +56,7 @@ const Single = () => {
         </div>
         <div className="bottom">
         <h1 className="title">Last Transactions</h1>
-          <List/>
+          {/* <List/> */}
         </div>
       </div>
     </div>
